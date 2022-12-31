cf48cf62d36fb01ed77163f4731f058ddae42276
Jamie Fabris <jf_22_fabris@hotmail.com>
2022-12-31 21:51:55 +0800

    31/12/2022

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from "react";
+import "./App.css";
 
 function Login() {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
   return (
-    <div>Login</div>
-  )
+    <div>
+      <form>
+        <Typography variant="h4" padding={3} textAlign="center">
+          Sign in
+        </Typography>
+
+        <TextField
+          margin="normal"
+          type={"text"}
+          variant="outlined"
+          placeholder="Username"
+        />
+
+        <TextField
+          margin="normal"
+          type={"email"}
+          variant="outlined"
+          placeholder="Email"
+        />
+        <TextField
+          margin="normal"
+          type={"password"}
+          variant="outlined"
+          placeholder="Password"
+        />
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
