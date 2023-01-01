ddf7798beb1f3d0dd200ceaf46711e013b804275
Jamie Fabris <jf_22_fabris@hotmail.com>
2023-01-02 01:07:41 +0800

    sign-up-form

---
+++ src/Input.js
@@ -0,0 +1,11 @@
+function Input({ id, label, placeholder, type, register, errorMessage }) {
+  return (
+    <div className="input-group">
+      <label htmlFor={id}>{label}</label>
+      <input type={type} id={id} placeholder={placeholder} {...register} />
+      <span className="error-message">{errorMessage}</span>
+    </div>
+  );
+}
+
+export default Input;
